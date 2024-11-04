7b8adc680697ad8c876d8baa49e324745f2a7a1f
takeokunn <bararararatty@gmail.com>
2024-11-04 11:42:54 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -4,22 +4,40 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, systems }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      treefmt-nix,
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = { pkgs, ... }:
+      imports = [ treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
         let
           pname = "cl-competitive";
           myLib = pkgs.sbcl.buildASDFSystem {
             inherit pname;
             version = "0.0.1";
             src = ./.;
-            systems = [ pname "${pname}/tests" ];
+            systems = [
+              pname
+              "${pname}/tests"
+            ];
             lispLibs = with pkgs.sbcl.pkgs; [ fiveam ];
           };
           lisp = pkgs.sbcl.withPackages (ps: [ myLib ]);
-        in {
+        in
+        {
           devShells.default = pkgs.mkShell { packages = with pkgs; [ lisp ]; };
           apps = {
             test = {
@@ -27,6 +45,15 @@
               program = pkgs.writeShellScriptBin "${pname}-test" ''
                 ${lisp}/bin/sbcl --noinform --non-interactive --eval "(require :asdf)" --eval "(asdf:test-system :${pname})"
               '';
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              actionlint.enable = true;
+              nixfmt.enable = true;
+              yamlfmt.enable = true;
             };
           };
         };
